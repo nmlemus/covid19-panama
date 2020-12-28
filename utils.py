94f5bd5f062e9f884e3453567eee795efc1cdfbb
Noel Moreno Lemus <nmlemus@gmail.com>
2020-12-28 03:02:35 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -34,7 +34,7 @@ def calculo_rt(st):
     
     @st.cache
     def load_data():
-        casos_panama = pd.read_excel('laprensa_casos_pruebas_time_series.xlsx')
+        casos_panama = pd.read_csv('laprensa_casos_pruebas_time_series.csv')
         return casos_panama
 
     # Create a text element and let the reader know the data is loading.
